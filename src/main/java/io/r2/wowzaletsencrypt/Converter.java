de84e93633d5e907a95dfb9cf7e2ee4bce4310fd
Robert Elek <r@r2.io>
2017-03-09 10:16:07 +0700

    first release, 0.1

---
+++ src/main/java/io/r2/wowzaletsencrypt/Converter.java
@@ -0,0 +1,7 @@
+package io.r2.wowzaletsencrypt;
+
+/**
+ * Created by robymus on 2017-03-09.
+ */
+public class Converter {
+}
---
+++ src/test/java/io/r2/wowzaletsencrypt/ConverterTest.java
@@ -0,0 +1,8 @@
+import static org.testng.Assert.*;
+
+/**
+ * Created by robymus on 2017-03-09.
+ */
+public class ConverterTest {
+
+}
