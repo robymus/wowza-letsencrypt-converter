42588a0be2cf530acc951bc42e417915b2d89633
Robert Elek <r@r2.io>
2018-07-05 09:49:18 +0200

    added test cases for empty lines in pem files, for issue #4

--- src/test/java/io/r2/wowzaletsencrypt/PemCertKeyTest.java
+++ src/test/java/io/r2/wowzaletsencrypt/PemCertKeyTest.java
@@ -13,13 +13,49 @@ import static org.assertj.core.api.Assertions.*;
 /**
  * Tests PemCertKey
  * Imported from https://github.com/robymus/simple-pem-keystore
+ * Added extra tests for trailing new lines
  */
 public class PemCertKeyTest {
 
+    @Test
+    public void testCert() throws Exception {
+        doTestCert("src/test/resources/pem/cert.pem");
+    }
 
     @Test
-    public void testCertOnly() throws Exception {
-        InputStream in = new FileInputStream("src/test/resources/pem/cert.pem");
+    public void testCertBlankLine() throws Exception {
+        doTestCert("src/test/resources/pem/cert-with-trailing-blank-line.pem");
+    }
+
+    @Test
+    public void testKey() throws Exception {
+        doTestKey("src/test/resources/pem/key.pem");
+    }
+
+    @Test
+    public void testKeyBlankLine() throws Exception {
+        doTestKey("src/test/resources/pem/key-with-two-trailing-blank-lines.pem");
+    }
+
+    @Test
+    public void testCertKey() throws Exception {
+        doTestCertKey(
+            "src/test/resources/pem/certchain.pem",
+            "src/test/resources/pem/key.pem"
+        );
+    }
+
+    @Test
+    public void testCertKeyBlankLine() throws Exception {
+        doTestCertKey(
+                "src/test/resources/pem/certchain-with-trailing-blank-line.pem",
+                "src/test/resources/pem/key-with-two-trailing-blank-lines.pem"
+        );
+    }
+
+
+    public void doTestCert(String fn) throws Exception {
+        InputStream in = new FileInputStream(fn);
         PemCertKey t = new PemCertKey(in);
 
         Certificate cert = t.getCertificate();
@@ -39,9 +75,8 @@ public class PemCertKeyTest {
         assertThat(t.getCreationDate()).isCloseTo(new Date(), 5000);
     }
 
-    @Test
-    public void testKeyOnly() throws Exception {
-        InputStream in = new FileInputStream("src/test/resources/pem/key.pem");
+    public void doTestKey(String fn) throws Exception {
+        InputStream in = new FileInputStream(fn);
         PemCertKey t = new PemCertKey(in);
 
         assertThat(t.hasCertificate()).isFalse();
@@ -58,12 +93,8 @@ public class PemCertKeyTest {
     }
 
 
-    @Test
-    public void testCertKey() throws Exception {
-        InputStream in = MultiFileConcatSource.fromFiles(
-                "src/test/resources/pem/certchain.pem",
-                "src/test/resources/pem/key.pem"
-        ).build();
+    public void doTestCertKey(String fn_cert, String fn_key) throws Exception {
+        InputStream in = MultiFileConcatSource.fromFiles(fn_cert, fn_key).build();
         PemCertKey t = new PemCertKey(in);
 
         Certificate cert = t.getCertificate();
@@ -84,6 +115,5 @@ public class PemCertKeyTest {
 
         assertThat(t.getCreationDate()).isCloseTo(new Date(), 5000);
     }
-
 
 }
